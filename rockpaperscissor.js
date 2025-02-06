f7d382f329cdee2226268cd07d5b05a451e97c0d
aryav <104564110+aryav-hash@users.noreply.github.com>
2025-02-06 21:54:10 +0530

    Created the getHumanChoice function.

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -11,4 +11,49 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function rock(computer) {
+    if (computer == "SCISSOR") {
+        console.log("The player WINS !");
+    }
+    else {
+        console.log("The player LOOSES !");
+    }
+}
+
+function paper(computer) {
+    if (computer == "ROCK") {
+        console.log("The player WINS !");
+    }
+    else {
+        console.log("The player LOOSES !");
+    }
+}
+
+function scissor(computer) {
+    if (computer == "ROCK") {
+        console.log("The player LOOSES !");
+    }
+    else {
+        console.log("The player WINS !");
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("What is your choice ?");
+    let generated = getComputerChoice();
+
+    if (choice.toUpperCase == generated) {
+        console.log("It's a TIE !");
+    }
+    else if (choice.toUpperCase == "ROCK") {
+        rock(generated);
+    }
+    else if (choice.toUpperCase == "SCISSOR") {
+        paper(generated);
+    }
+    else if (choice.toUpperCase == "PAPER") {
+        scissor(generated);
+    }
+}
+
+getHumanChoice();
