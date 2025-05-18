03795bc83504e3d90cbe7da034796c9e097ff48e
aryav <104564110+aryav-hash@users.noreply.github.com>
2025-05-18 19:48:09 +0530

    feat: Began to build the layout and styling of the page for the todo app.

--- ToDoApp/src/App.jsx
+++ ToDoApp/src/App.jsx
@@ -1,6 +1,16 @@
 function App() {
   return (
     <>
+      <div className="flex justify-center w-full h-screen bg-rose-400">
+        <div className="mt-10 h-100 w-[1400px] bg-rose-300">
+          <div className="flex justify-center p-10 font-mono">
+            <h1 className="text-5xl font-bold text-indigo-600">To Do List</h1>
+          </div>
+          <div>
+
+          </div>
+        </div>
+      </div>
       
     </>
   )
